b253da9501dca7fb9a13e334adac3dd32bf2bd87
AnnaVeller <anna.ovsi@mail.ru>
2021-06-03 00:12:53 +0300

    make touch for phone (try 1)

--- assets/src/app.js
+++ assets/src/app.js
@@ -31,6 +31,25 @@ const END_TEXT = "ТЫ ВЫИГРАЛ";
 const LOSE_TEXT = "ТЫ ПРОИГРАЛ"
 const RECORD_BEGIN = 1000;
 
+var touchstartX = 0;
+var touchstartY = 0;
+var touchendX = 0;
+var touchendY = 0;
+
+var gesuredZone = document.querySelector('canvas');
+
+gesuredZone.addEventListener('touchstart', event => {
+    touchstartX = event.screenX;
+    touchstartY = event.screenY;
+}, false);
+
+gesuredZone.addEventListener('touchend', event => {
+    touchendX = event.screenX;
+    touchendY = event.screenY;
+    handleGesure();
+}, false);
+
+
 let game;
 let record = RECORD_BEGIN;
 
@@ -73,3 +92,23 @@ class App {
 
 
 App.init();
+
+
+function handleGesure() {
+    var swiped = 'swiped: ';
+    if (touchendX < touchstartX) {
+        alert(swiped + 'left!');
+    }
+    if (touchendX > touchstartX) {
+        alert(swiped + 'right!');
+    }
+    if (touchendY < touchstartY) {
+        alert(swiped + 'down!');
+    }
+    if (touchendY > touchstartY) {
+        alert(swiped + 'left!');
+    }
+    if (touchendY == touchstartY) {
+        alert('tap!');
+    }
+}
